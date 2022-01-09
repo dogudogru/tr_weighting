fa29c6f86fc7e393f5104acb1f36579ff6bc94c9
Doğukan Doğru <dogukandogru1@gmail.com>
2022-01-09 13:57:31 +0300

    def

---
+++ streamlit_app.py
@@ -0,0 +1,278 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import base64
+
+st.set_page_config(layout='wide')
+
+main_bg = "background.png"
+main_bg_ext = "png"
+
+st.markdown(
+    f"""
+    <style>
+    .reportview-container {{
+        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+    }}
+    .sidebar .sidebar-content {{
+        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+@st.cache
+
+def convert_df(df):
+
+     return df.to_csv().encode('utf-8')
+
+def main():
+    cs_sidebar()
+    cs_main_calc()
+    cs_text()
+
+def cs_sidebar():
+    
+    st.sidebar.write("Anket verisini yükleyin")
+
+    return None
+
+st.title('Otomatik Ağırlıklandırma')
+
+def cs_main_calc():
+
+    uploaded_file = st.sidebar.file_uploader("",type=['xlsx'],accept_multiple_files=False,key="fileUploader")
+
+    if uploaded_file is not None:
+
+        df = pd.read_excel(uploaded_file)
+
+        df['educ_realprct'] = df['educ']
+        df['educ_real'] = df['educ']    
+
+        sex_pop = pd.DataFrame({'sex':['Kadın','Erkek'], 'sex_weight':[0.501716178,0.498283822]})
+        age_pop = pd.DataFrame({'age':['18-24','25-34','35-44','45-54','55-64','65 ve üstü'], 'age_weight':[0.155496,0.211942377,0.210170714,0.169884995,0.129716679,0.122789]})
+        educ_pop = pd.DataFrame({'educ':['Okuma-yazma bilmiyor','İlkokul terk','İlkokul mezunu','Ortaokul veya dengi meslek ortaokul mezunu','Lise ve dengi meslek okulu mezunu','Yüksekokul veya üniversite mezunu','Yüksek lisans','Doktora'], 'educ_weight':[0.276159321,0.276159321,0.276159321,0.535093254,0.535093254,0.188747425,0.188747425,0.188747425]})
+        party_pop = pd.DataFrame({'2018_party':['Adalet ve Kalkınma Partisi (AKP)','Cumhuriyet Halk Partisi (CHP)','Diğer','Halkların Demokratik Partisi (HDP)','İYİ Parti','Oy kullanmadım < 21','Oy kullanmadım > 20','Saadet Partisi','Bağımsız aday','Milliyetçi Hareket Partisi (MHP)'], 'party_weight':[0.339423,0.180605,0.004301,0.093328,0.0794286,0.08852,0.146821,0.010691,0.001203,0.088524]})
+
+        df['all'] = len(df['sex']) # Toplam katilimci sayisini belirten kolon
+
+        df['count_sex'] = (df.groupby('sex')['sex'].transform('count')) #Cinsiyet kirilimli sayim
+        df['count_age'] = (df.groupby('age')['age'].transform('count')) #Yas kirilimli sayim
+        df['count_educ']= (df.groupby('educ')['educ'].transform('count')) #Egitim kirilimli sayim
+
+        df['sex_prct'] = df['count_sex']  / df['all'] #Orneklem dagilimini buluyorum
+        df['age_prct'] = df['count_age']  / df['all'] #Orneklem dagilimini buluyorum
+        df['educ_prct']= df['count_educ'] / df['all'] #Orneklem dagilimini buluyorum
+
+        df = df.merge(sex_pop, on='sex', how= 'left') #Populasyon bilgilerini bagliyorum 
+        df = df.merge(age_pop, on='age', how= 'left') #Populasyon bilgilerini bagliyorum
+        df = df.merge(educ_pop, on='educ', how = 'left') #Populasyon bilgilerini bagliyorum
+        df = df.merge(party_pop, on='2018_party', how = 'left') #Populasyon bilgilerini bagliyorum
+
+
+        df['educ_realprct']= df['educ_realprct'].replace({'Okuma-yazma bilmiyor': 'ilk', 
+                                        'İlkokul terk': 'ilk',
+                                        'İlkokul mezunu': 'ilk',
+                                        'Ortaokul veya dengi meslek ortaokul mezunu': 'orta_lise',
+                                        'Lise ve dengi meslek okulu mezunu': 'orta_lise',
+                                        'Yüksekokul veya üniversite mezunu': 'uni',
+                                        'Yüksek lisans': 'uni',
+                                        'Doktora': 'uni',
+                                        })
+
+        df['educ_real']= df['educ_real'].replace({'Okuma-yazma bilmiyor': 'İlköğretim veya daha az', 
+                                        'İlkokul terk': 'İlköğretim veya daha az',
+                                        'İlkokul mezunu': 'İlköğretim veya daha az',
+                                        'Ortaokul veya dengi meslek ortaokul mezunu': 'Ortaokul',
+                                        'Lise ve dengi meslek okulu mezunu': 'Lise',
+                                        'Yüksekokul veya üniversite mezunu': 'Üniversite veya daha fazla',
+                                        'Yüksek lisans': 'Üniversite veya daha fazla',
+                                        'Doktora': 'Üniversite veya daha fazla',
+                                        })
+
+        #burada agirliklandirma icin kullanacagimiz birlestirilmis egitim gruplarini olusturuyorum
+
+        ed_a = df.loc[df['educ'] == 'Okuma-yazma bilmiyor', 'educ_prct'].iloc[0]
+        ed_b = df.loc[df['educ'] == 'İlkokul terk', 'educ_prct'].iloc[0]
+        ed_c = df.loc[df['educ'] == 'İlkokul mezunu', 'educ_prct'].iloc[0]
+        ed_d = df.loc[df['educ'] == 'Ortaokul veya dengi meslek ortaokul mezunu', 'educ_prct'].iloc[0]
+        ed_e = df.loc[df['educ'] == 'Lise ve dengi meslek okulu mezunu', 'educ_prct'].iloc[0]
+        ed_f = df.loc[df['educ'] == 'Yüksekokul veya üniversite mezunu', 'educ_prct'].iloc[0]
+        ed_g = df.loc[df['educ'] == 'Yüksek lisans', 'educ_prct'].iloc[0]
+        ed_h = df.loc[df['educ'] == 'Doktora', 'educ_prct'].iloc[0]
+
+        ed_ilk = ed_a + ed_b + ed_c
+        ed_orta_lise = ed_d + ed_e
+        ed_uni = ed_f + ed_g + ed_h
+
+        df['educ_realprct'] = df['educ_realprct'].replace('ilk',ed_ilk).replace('orta_lise',ed_orta_lise).replace('uni',ed_uni)
+
+
+        df['w_sex'] =  df['sex_weight'] /  df['sex_prct']
+        df['w_age'] = df['age_weight']  / df['age_prct'] 
+        df['w_educ'] = df['educ_weight'] / df['educ_realprct']  #educ_prct yerine educ_realprct kullandigimiz yer
+
+        df['total'] = df['w_sex'] * df['w_age'] * df['w_educ'] 
+
+        df['std'] = df.loc[:,"total"].std()
+        df['avg'] = df.loc[:,"total"].mean()
+        df['thold'] = df['std'] * 2 + df['avg']
+        df = df.drop(columns=['std', 'avg'])
+
+        calc = np.where(df['total'] > df['thold'], df['thold'], df['total'])
+        df['ilk_agirliklar'] = calc
+
+        party_pivot = pd.pivot_table(df,
+                                    index='2018_party',
+                                    values='ilk_agirliklar',
+                                    aggfunc=np.sum).reset_index()
+
+        # party_pivot = party_pivot.columns.get_level_values(0)
+
+        party_pivot = party_pivot.rename({'ilk_agirliklar': 'first_weight'}, axis=1)
+
+        party_pivot['all'] = df['ilk_agirliklar'].sum()
+        
+        party_pivot['party_prct'] = party_pivot['first_weight']  / party_pivot['all']
+
+        df = df.merge(party_pivot, on='2018_party', how= 'left')
+
+        df['w_party'] =  df['party_weight'] / df['party_prct'] 
+
+        df['total2'] = df['w_party'] * df['ilk_agirliklar']
+
+        df['std2'] = df.loc[:,"total2"].std()
+        df['avg2'] = df.loc[:,"total2"].mean()
+        df['thold2'] = df['std2'] * 2 + df['avg2']
+        df = df.drop(columns=['std2', 'avg2'])
+
+        calc2 = np.where(df['total2'] > df['thold2'], df['thold2'], df['total2'])
+        df['Duzeltilmis_Agirlik'] = calc2
+
+        min_weight = df['Duzeltilmis_Agirlik'].min()
+        max_weight = df['Duzeltilmis_Agirlik'].max()
+        std_weight = df['Duzeltilmis_Agirlik'].std() 
+        mean_weight = df['Duzeltilmis_Agirlik'].mean()
+
+        dataframe_x = pd.DataFrame({
+                '-': ["Minimum Ağırlık", "Maksimum Ağırlık", "Standart Sapma", "Ortalama Ağırlık"],
+                '': [min_weight, max_weight, std_weight, mean_weight],
+            })
+        dataframe_x.set_index('-', inplace=True)
+
+        st.write("Ağırlık özeti aşağıdaki gibidir",dataframe_x)
+
+        st.header("Ağırlıklandırma Özeti")
+        
+        sex_dist_w = df.pivot_table(values='Duzeltilmis_Agirlik',
+                    index='sex',
+                    aggfunc=np.sum,
+                    fill_value=0,
+                    margins=True,
+                    margins_name='Genel Toplam')
+
+        sex_dist_p = round(sex_dist_w*100/sex_dist_w.iloc[-1,:],2).astype(str) + "%"
+
+        #sex_dist = pd.concat([sex_dist_w,sex_dist_p], axis=1,ignore_index=True)
+        
+        st.write("**Cinsiyet Dağılımı**")
+
+        st.write(sex_dist_p)
+
+        educ_dist_w = df.pivot_table(values='Duzeltilmis_Agirlik',
+                    index='educ',
+                    aggfunc=np.sum,
+                    fill_value=0,
+                    margins=True,
+                    margins_name='Genel Toplam')
+
+        educ_dist_p = round(educ_dist_w*100/educ_dist_w.iloc[-1,:],2).astype(str) + "%"
+
+        educ_dist_w_2 = df.pivot_table(values='Duzeltilmis_Agirlik',
+                    index='educ_real',
+                    aggfunc=np.sum,
+                    fill_value=0,
+                    margins=True,
+                    margins_name='Genel Toplam')
+
+        educ_dist_p_2 = round(educ_dist_w_2*100/educ_dist_w_2.iloc[-1,:],2).astype(str) + "%"
+
+        col1, col2 = st.columns(2)
+
+        col1.write("**Eğitim Dağılımı**")
+
+        col1.write(educ_dist_p)
+
+        col2.write("**Tablolarda Kullanılan Eğitim Grubu Dağılımı**")
+
+        col2.write(educ_dist_p_2)
+
+
+        age_dist_w = df.pivot_table(values='Duzeltilmis_Agirlik',
+                    index='age',
+                    aggfunc=np.sum,
+                    fill_value=0,
+                    margins=True,
+                    margins_name='Genel Toplam')
+
+        age_dist_p = round(age_dist_w*100/age_dist_w.iloc[-1,:],2).astype(str) + "%"
+
+        #sex_dist = pd.concat([sex_dist_w,sex_dist_p], axis=1,ignore_index=True)
+        
+        st.write("**Yaş Dağılımı**")
+
+        st.write(age_dist_p)
+
+
+        party_dist_w = df.pivot_table(values='Duzeltilmis_Agirlik',
+                    index='2018_party',
+                    aggfunc=np.sum,
+                    fill_value=0,
+                    margins=True,
+                    margins_name='Genel Toplam')
+
+        party_dist_p = round(party_dist_w*100/party_dist_w.iloc[-1,:],2).astype(str) + "%"
+
+        #sex_dist = pd.concat([sex_dist_w,sex_dist_p], axis=1,ignore_index=True)
+        
+        st.write("**2018'de Tercih Edilen Parti Dağılımı**")
+
+        st.write(party_dist_p)
+
+        
+
+
+        #Veriyi indirmek icin
+        csv = convert_df(df)
+        
+
+        st.download_button(
+        label="Ağırlıklandırılmış veriyi CSV dosyası olarak indirebilirsiniz",
+        data=csv,
+        file_name='Turkiye_Raporu_Agirliklandirilmis.csv',
+        mime='text/csv')
+
+    
+
+
+    else:
+        st.warning("Anket verisini Excel dosyası olarak yan taraftaki butondan yüklemeniz gerekir.")
+
+
+    return None
+
+
+def cs_text():
+
+    st.header('Veriyi Hazırlama')
+    st.markdown('Veriyi yüklemeden önce bazı sütunların isimlerini değiştirmeniz gerekir!')
+    sampledf = pd.DataFrame({'Old':['Katılımcının cinsiyeti','Katılımcının yaş grubu (18-24 vs.)','Katılımcının eğitim seviyesi','Katılımcının 2018 seçimlerinde oy verdiği parti'], 'New':['sex','age','educ','2018_party']})
+    sampledf.set_index('Old', inplace=True)
+    st.dataframe(sampledf)
+    return None
+
+if __name__ == '__main__':
+    main()
